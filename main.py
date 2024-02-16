8834f5d6989cb1990bb120e83ad62fe02dcc1f79
Grace Wanjiku <wanjiku.grace9@gmail.com>
2024-02-16 13:19:07 +0300

    add a pages folder

--- main.py
+++ main.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title='Home',
+    page_icon=':)',
+    layout= 'wide'
+)
 st.title("Churn Prediction App")
 st.button("Login")
 st.text_input("Enter your name")
